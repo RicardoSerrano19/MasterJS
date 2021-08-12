59c48dfb1e32529c7ef8d686546bfbf016126222
Ricardo Serrano <ricardo.ss@live.com>
2021-08-12 09:47:58 -0500

    Objects.js

--- EloquentJS/06-Objects/Objects.js
+++ EloquentJS/06-Objects/Objects.js
@@ -18,6 +18,7 @@ whiteRabbit.speak('Oh my ears and whiskers, ' + "how late it's getting!");
 hungryRabbit.speak('I could use a carrot right now.');
 
 speak.call(hungryRabbit, 'Burp!');
+whiteRabbit;
 function normalize() {
   console.log(this.coords.map((n) => n / this.length));
 }
@@ -58,3 +59,77 @@ Rabbit.prototype.speak = function (line) {
 let weirdRabbit = new Rabbit('weird');
 weirdRabbit.speak('Hola soy el pana rabbit');
 weirdRabbit;
+
+console.log(Object.getPrototypeOf(Rabbit) == Function.prototype);
+console.log(Object.getPrototypeOf(weirdRabbit) == Rabbit.prototype);
+
+// 📃 Class Notation
+class RabbitClass {
+  constructor(type) {
+    this.type = type;
+  }
+  speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
+  }
+}
+
+let killerRabbitC = new RabbitClass('killer');
+let blackRabbitC = new RabbitClass('black');
+killerRabbitC;
+
+let object = new (class {
+  getWord() {
+    return 'hello';
+  }
+})();
+object.getWord();
+
+// 📃 Overriding derived properties
+RabbitClass.prototype.teeth = 'small';
+console.log('1 ->' + killerRabbitC.teeth);
+// → small
+killerRabbitC.teeth = 'long, sharp, and bloody';
+console.log('2 ->' + killerRabbitC.teeth);
+// → long, sharp, and bloody
+console.log('3 ->' + blackRabbitC.teeth);
+// → small
+console.log('4 ->' + RabbitClass.prototype.teeth);
+// → small
+
+console.log(Array.prototype.toString == Object.prototype.toString);
+// → false
+console.log([1, 2].toString());
+// → 1,2
+
+console.log(Object.prototype.toString.call([1, 2]));
+// → [object Array]
+
+// 📃 Maps
+let agesObject = {
+  Boris: 39,
+  Liang: 22,
+  Julio: 62,
+}; //-> Ages object derivated from Object.prototype
+console.log(`Julio is ${agesObject['Julio']} ages`);
+console.log('Is Jacks age known?', 'Jack' in agesObject);
+console.log('Is toStrings age known?', 'toString' in agesObject);
+console.log('toString' in Object.create(null));
+
+let ages = new Map();
+ages.set('Boris', 39);
+ages.set('Liang', 22);
+ages.set('Julio', 22);
+console.log(`Julio is ${ages.get('Julio')} ages`);
+console.log("Is Jack's age known?", ages.has('Jack'));
+console.log(ages.has('toString'));
+
+console.log({ x: 1 }.hasOwnProperty('x'));
+console.log({ x: 1 }.hasOwnProperty('toString'));
+
+// 📃 POlymorphism
+RabbitClass.prototype.toString = function () {
+  return `a ${this.type} rabbit`;
+};
+console.log(String(blackRabbitC));
+
+// 📃 SYmbols
