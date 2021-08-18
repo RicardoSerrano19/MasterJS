bc55f2cfe9e7179891a05ee16954e3d6b07c79fe
Ricardo Serrano <ricardo.ss@live.com>
2021-08-18 12:15:14 -0500

    Add class to listaMensajes

--- PruebasTecnicas/EntrevistaVW/main.js
+++ PruebasTecnicas/EntrevistaVW/main.js
@@ -1,6 +1,7 @@
 //TODO
 const txtInput = document.querySelector('#txtInput');
 const btnGuardar = document.querySelector('#btnGuardar');
+const listaMensajes = document.querySelector('#listaMensajes');
 
 btnGuardar.addEventListener('click', (e) =>{
     e.preventDefault();
