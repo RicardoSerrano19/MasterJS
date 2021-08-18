9e6e0fe68c0da6c487c08d994a9b723d8f066ea6
Ricardo Serrano <ricardo.ss@live.com>
2021-08-18 12:08:19 -0500

    Add querySelector to text and button and it event listener

--- PruebasTecnicas/EntrevistaVW/main.js
+++ PruebasTecnicas/EntrevistaVW/main.js
@@ -1,1 +1,7 @@
 //TODO
+const txtInput = document.querySelector('#txtInput');
+const btnGuardar = document.querySelector('#btnGuardar');
+
+btnGuardar.addEventListener('click', (e) =>{
+    e.preventDefault();
+})
