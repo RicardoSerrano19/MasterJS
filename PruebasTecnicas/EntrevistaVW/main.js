f954bfb46a844c3e1bc7624feaee0ff409124047
Ricardo Serrano <ricardo.ss@live.com>
2021-08-18 19:03:21 -0500

    Add eslint

--- PruebasTecnicas/EntrevistaVW/main.js
+++ PruebasTecnicas/EntrevistaVW/main.js
@@ -5,12 +5,12 @@ const listaMensajes = document.querySelector('#listaMensajes');
 const convertirCapitalize = document.querySelector('#convertirCapitalize');
 
 btnGuardar.addEventListener('click', (e) =>{
-    e.preventDefault();
-    const listElement = document.createElement('li');
-    const textNode = document.createTextNode(txtInput.value);
-    listElement.appendChild(textNode);
-    listaMensajes.appendChild(listElement);
-    txtInput.value = "";
+	e.preventDefault();
+	const listElement = document.createElement('li');
+	const textNode = document.createTextNode(txtInput.value);
+	listElement.appendChild(textNode);
+	listaMensajes.appendChild(listElement);
+	txtInput.value = '';
 });
 
 /*convertirCapitalize.addEventListener('click', (e) =>{
@@ -18,14 +18,14 @@ btnGuardar.addEventListener('click', (e) =>{
 });
 */
 
-convertirCapitalize.addEventListener('click', (e) =>{
-   const list = listaMensajes.children;
-   for(let element of list){
-       let listWords = element.textContent.trim().split(" ")
-            .filter(word => word != "")
-            .map(word => {
-                return word.toString().charAt(0).toUpperCase() + word.substr(1);
-            });
-        element.textContent = listWords.join(' ');
-   }
+convertirCapitalize.addEventListener('click', () =>{
+	const list = listaMensajes.children;
+	for(let element of list){
+		let listWords = element.textContent.trim().split(' ')
+			.filter(word => word != '')
+			.map(word => {
+				return word.toString().charAt(0).toUpperCase() + word.substr(1);
+			});
+		element.textContent = listWords.join(' ');
+	}
 });
