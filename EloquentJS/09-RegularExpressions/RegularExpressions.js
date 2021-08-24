9b0fd3cbf75546da5d5bb85062f70dc60b92249f
Ricardo Serrano <ricardo.ss@live.com>
2021-08-24 14:08:10 -0500

    feat(chapter09): Sets of characters

--- EloquentJS/09-RegularExpressions/RegularExpressions.js
+++ EloquentJS/09-RegularExpressions/RegularExpressions.js
@@ -18,3 +18,31 @@ whether the string contains a match of the pattern in the expression.
 
 console.log(/abc/.test('dabcde')); // → true
 console.log(/abc/.test('abxde')); // → false
+
+/* 📃 Sets of characters
+  # Regular expression objects have a number of methods. The simplest
+one is test . If you pass it a string, it will return a Boolean telling you
+whether the string contains a match of the pattern in the expression.
+ - Regular expression can occurs anywhere in the string
+*/
+
+console.log(/[0123456789]/.test('in 1992')); // → true
+// 🧩 [] → Match any of the characters inside brackets, dont matter the position
+console.log(/[0-9]/.test('in 1992')); // → true
+// 🧩 [min-max] → Match any of the characters in the range of characters
+// 🧩 \d Any digit character
+// 🧩 \w An alphanumeric character (“word character”)
+// 🧩 \s Any whitespace character (space, tab, newline, and similar)
+// 🧩 \D A character that is not a digit
+// 🧩 \W A nonalphanumeric character
+// 🧩 \S A nonwhitespace character
+// 🧩 . A nonwhitespace character
+
+let dateTime = /\d\d-\d\d-\d\d\d\d \d\d:\d\d/; // → Match a date and time like 19-06-1997 12:20
+console.log(dateTime.test('01-30-2003 15:20')); // → true
+console.log(dateTime.test('30-jan-2003 15:20')); // → false
+
+let notBinary = /[^01]/;
+console.log(notBinary.test('110010001000101000110')); // → false
+console.log(notBinary.test('110010001000121000110')); // → true
+// 🧩 [^] → Any character except the ones in the set
