61c725d93ff189918738104bde1f7943adf4e38e
Ricardo Serrano <ricardo.ss@live.com>
2021-08-24 12:24:34 -0500

    feat(chapter09): Creating a Regular Expression

---
+++ EloquentJS/09-RegularExpressions/RegularExpressions.js
@@ -0,0 +1,10 @@
+/* 📃 Creating a Regular Expression
+  # A regular expression is a type of object. It can be either constructed
+with the RegExp constructor or written as a literal value by enclosing a
+pattern in forward slash ( / ) characters.
+*/
+
+let re1 = new RegExp("abc");
+let re2 = /abc/;
+// re1 and re2 represent the same pattern: 
+// 🧩 abc → 'a' character followed by 'b' followed by a 'c' 
