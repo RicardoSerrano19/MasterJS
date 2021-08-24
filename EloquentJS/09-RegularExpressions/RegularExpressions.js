a1f1cf1f9d0da495af7e76d4d31787e7c60eeea8
Ricardo Serrano <ricardo.ss@live.com>
2021-08-24 12:28:16 -0500

    feat(chapter09): Testing for matches

--- EloquentJS/09-RegularExpressions/RegularExpressions.js
+++ EloquentJS/09-RegularExpressions/RegularExpressions.js
@@ -4,7 +4,17 @@ with the RegExp constructor or written as a literal value by enclosing a
 pattern in forward slash ( / ) characters.
 */
 
-let re1 = new RegExp("abc");
+let re1 = new RegExp('abc');
 let re2 = /abc/;
-// re1 and re2 represent the same pattern: 
-// 🧩 abc → 'a' character followed by 'b' followed by a 'c' 
+// re1 and re2 represent the same pattern:
+// 🧩 abc → 'a' character followed by 'b' followed by a 'c'
+
+/* 📃 Testing for matches
+  # Regular expression objects have a number of methods. The simplest
+one is test . If you pass it a string, it will return a Boolean telling you
+whether the string contains a match of the pattern in the expression.
+ - Regular expression can occurs anywhere in the string
+*/
+
+console.log(/abc/.test('dabcde')); // → true
+console.log(/abc/.test('abxde')); // → false
