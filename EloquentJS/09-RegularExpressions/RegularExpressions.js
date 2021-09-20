213f9c7d01d49f4bc07f48e898bf2de88e0d4d2f
Ricardo Serrano <ricardo.ss@live.com>
2021-09-20 12:13:06 -0500

    feat(chapter09): Parsing an INI file

--- EloquentJS/09-RegularExpressions/RegularExpressions.js
+++ EloquentJS/09-RegularExpressions/RegularExpressions.js
@@ -271,4 +271,56 @@ while(matchLoop = numberLoop.exec(inputLoop)){
 
 // → Found 3 at 14
 // Found 42 at 33
-// Found 88 at 40
+// Found 88 at 40x
+
+/* 📃 Parsing an INI file
+  # Convert a string  into an object whose properties hold strings for settings written before the first section header and subobjects for sections, with those subobjects holding the section’s settings
+*/
+
+const information = `searchengine=https://duckduckgo.com/?q=$1
+spitefulness=9.7
+
+; los comentarios estan precedidos por un punto y coma...
+; cada seccion contiene un enemigo individual
+
+[larry]
+fullname=Larry Doe
+type=bravucon del preescolar
+website=http://www.geocities.com/CapeCanaveral/11451
+
+[davaeorn]
+fullname=Davaeorn
+type=hechizero malvado
+outpudir=/home/marijn/enemies/davaeorn`;
+
+function parseINI(string) {
+  // Start with an object to hold the top-level fields
+  let result = {};
+  let section = result;
+  string.split(/\r?\n/).forEach(line=>{
+    let match;
+    if (match = line.match(/^(\w+)=(.*)$/)) {
+      section[match[1]] = match[2];
+    } else if (match = line.match(/^\[(.*)\]$/)) {
+      section = result[match[1]] = {};
+    } else if (!/^\s*(;.*)?$/.test(line)) {
+      throw new Error("Line '" + line + "' is not valid.");
+    }
+  });
+  return result;
+}
+console.log(parseINI(information));
+/* -> {
+  searchengine: 'https://duckduckgo.com/?q=$1',
+  spitefulness: '9.7',
+  larry: {
+    fullname: 'Larry Doe',
+    type: 'bravucon del preescolar',
+    website: 'http://www.geocities.com/CapeCanaveral/11451'
+  },
+  davaeorn: {
+    fullname: 'Davaeorn',
+    type: 'hechizero malvado',
+    outpudir: '/home/marijn/enemies/davaeorn'
+  }
+} */
