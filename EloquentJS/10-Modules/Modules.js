6f7502145df7cfe24f2654f5702410c24cfeb23c
Ricardo Serrano <ricardo.ss@live.com>
2021-09-21 11:50:44 -0500

    feat(chapter10): Packages

--- EloquentJS/10-Modules/Modules.js
+++ EloquentJS/10-Modules/Modules.js
@@ -12,3 +12,16 @@
 
   The relations between modules are called *dependencies*. When module needs a piece from another module, it is said to depend on that module.
 */
+
+
+/* 📃 Packages
+  # One of the advantages of building a program out of separate pieces, and being actually able to run those pieces on their own, is that you might be able to apply the same piece in different programs.
+
+  A *package* is a chunk of code that can be distributed (copied and installed). It may contain one or more modules and has information about wich other packeges it, depends on.
+
+  When a problem is found in a package or a new features is added, the package is updated. Now the programs that depend on it can upgrade to the new version.
+
+  NPM (Node Packages Module), is a online service where one can download (and upload) packages and a program (bundle with Node.js) that helps you install and manage them. 
+
+  Software is cheap to copy, so once someone has wirtten it, distributing it to other people is an efficient process.
+*/
