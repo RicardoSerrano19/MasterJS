564470af0065bfa6733cf6c26af4347c1189f17c
Ricardo Serrano <ricardo.ss@live.com>
2021-09-24 13:31:23 -0500

    feat(chapter11): Networks are hard

--- EloquentJS/11-AsynchronousProgramming/AsynchronousProgramming.js
+++ EloquentJS/11-AsynchronousProgramming/AsynchronousProgramming.js
@@ -78,3 +78,40 @@ new Promise((_, reject) => reject(new Error('Fail')))
 
 // → Caught failure Error: Fail
 // → Handler 2 nothing
+
+
+/* 📃 Networks are hard
+  #
+*/
+
+class Timeout extends Error{}
+
+function request(nest, target, type, content){
+  return new Promise((resolve, reject) => {
+    let done = false;
+    function attempt(n){
+      nest.send(target, type, content, (failed, value) =>{
+        done = true;
+        if(failed) reject(failed);
+        else resolve (value);
+      });
+      setTimeout(() => {
+        if (done) return;
+        else if(n < 3) attempt(n+1);
+        else reject(new Timeout('Timed out'));
+      }, 250);
+    }
+    attempt(1);
+  })
+}
+
+function requestType(name, handler){
+  defineRequestType(name, (nest, content, source, callback) =>{
+    try{
+      Promise.resolve(handler(nest, content, source))
+        .then(response => callback(null, response), failure => callback(failure));
+    }catch (exception){
+      callback(exception);
+    }
+  });
+}
