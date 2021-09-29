15dfeaa75bfb49b5136fb65195adfa9ed84f6315
Ricardo Serrano <ricardo.ss@live.com>
2021-09-29 13:39:59 -0500

    feat(chapter11): NEtwork flooding

--- EloquentJS/11-AsynchronousProgramming/AsynchronousProgramming.js
+++ EloquentJS/11-AsynchronousProgramming/AsynchronousProgramming.js
@@ -134,3 +134,28 @@ function availableNeighbors(nest) {
     return nest.neighbors.filter((_, i) => result[i]);
   });
 }
+
+
+/* 📃 Network Flooding
+  # 
+*/
+
+import { everywhere } from './crow-tech.js'
+
+everywhere(nest => {
+  nest.state.gossip = [];
+});
+
+function sendGossip(nest, message, exceptFor = null){
+  nest.state.gossip.push(message);
+  for(let neighbor of nest.neighbors){
+    if(neighbor == exceptFor) continue;
+    request(nest, neighbor, "gossip", message);
+  }
+}
+
+requestType("gossip", (nest, message, source) => {
+  if(nest.state.gossip.includes(message)) return;
+  console.log(`${nest.name} received gossip '${message}' from ${source}`);
+  sendGossip(nest, message, source);
+})
