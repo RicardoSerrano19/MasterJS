1e1d55c7031fded062f58e817154031758f1ff79
Ricardo Serrano <ricardo.ss@live.com>
2021-09-29 15:06:55 -0500

    feat(chapter11): Message Routing

--- EloquentJS/11-AsynchronousProgramming/AsynchronousProgramming.js
+++ EloquentJS/11-AsynchronousProgramming/AsynchronousProgramming.js
@@ -159,3 +159,61 @@ requestType("gossip", (nest, message, source) => {
   console.log(`${nest.name} received gossip '${message}' from ${source}`);
   sendGossip(nest, message, source);
 })
+
+
+/* 📃 Message Routing
+  # If a given node wants to talk to a single other node, flooding is not a very efficient approach.
+  Since each nest knows only about its direct neighbors, it doesn’t have the information it needs to compute a route. We must somehow spread the information about these connections to all nests, preferably in a way that allows it to change over time, when nests are abandoned or new nests are built.
+*/
+
+requestType('connections', (nest, {name, neighbors}, source) =>{
+  let connections = nest.state.connections;
+  if(JSON.stringify(connections.get(name)) == JSON.stringify(neighbors)) return;
+  connections.set(name, neighbors);
+  broadcastConnections(nest, name, source);
+})
+
+function broadcastConnections(nest, name, exceptFor = null){
+  for(let neighbor of nest.neighbors){
+    if(neighbor == exceptFor) continue;
+    request(
+      nest, 
+      neighbor, 
+      'connections', 
+      {name, neighbors: nest.state.connections.get(name)}
+    );
+  }
+}
+
+everywhere(nest => {
+  nest.state.connections = new Map();
+  nest.state.connections.set(nest.name, nest.neighbors);
+  broadcastConnections(nest, nest.name)
+})
+
+function findRoute(from, to, connections) {
+  let work = [{ at: from, via: null }];
+  for (let i = 0; i < work.length; i++) {
+    let { at, via } = work[i];
+    for (let next of connections.get(at) || []) {
+      if (next == to) return via;
+      if (!work.some(w => w.at == next)) {
+        work.push({ at: next, via: via || next });
+      }
+    }
+  }
+  return null;
+}
+
+function routeRequest(nest, target, type, content) {
+  if (nest.neighbors.includes(target)) {
+    return request(nest, target, type, content);
+  } else {
+    let via = findRoute(nest.name, target, nest.state.connections);
+    if (!via) throw new Error(`No route to ${target}`);
+    return request(nest, via, "route", { target, type, content });
+  }
+}
+requestType("route", (nest, { target, type, content }) => {
+  return routeRequest(nest, target, type, content);
+});
