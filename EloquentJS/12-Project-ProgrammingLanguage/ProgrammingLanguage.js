67e2e6ade0d272335ac520f92cfa13a540fa92b1
Ricardo Serrano <ricardo.ss@live.com>
2021-10-01 14:31:12 -0500

    feat(chapter12): Parsing

---
+++ EloquentJS/12-Project-ProgrammingLanguage/ProgrammingLanguage.js
@@ -0,0 +1,116 @@
+/* 📏 Overview
+    # Building your own programming language is surprisingly easy (as long as you dont 
+        aim to high) and very enlightening.
+        
+        We will build a programming language called Egg. It will be a tiny,
+        simple language—but one that is powerful enough to express any com-
+        putation you can think of. It will allow simple abstraction based on
+        functions
+*/
+/*
+    # 🔴 Parsing
+    The most immediately visible part of a programming language is its *syntax or notation*.
+    A *parser* is a program that reads a piece of text and produces a data structure that 
+    reflects the structure of the program contained in that text.
+    If the text does not form a valid program, the parser should point out the error.
+
+    - "value" -> Literal strings or number
+    - "word" -> Identifiers (names). Have a *name* property that holds the identifiers name
+    - "apply" -> Expressions represent applications. Have *operator* prop that refers
+                    to the expression that is being applied, and *args* property that
+                    holds an array of argument expressions.
+*/
+
+/*
+    Example: 
+        do(define(x, 10),
+        if(>(x, 5),
+        print("large"),
+        print("small")))
+    
+    The * >(x,5) * would be represented like this:
+    {
+        type: "apply",
+        operator: {type: "word", name: ">"},
+        args: [
+            {type: "word", name: "x"},
+            {type: "word", name: "5"}
+        ]
+    }
+
+    
+    The fact that expressions contain other expressions, wich in turn might contain more
+    expressions, is similar to the way tree branches split and split again
+
+    ● do
+    |
+    |-> ● define
+    |   |--> x
+    |   |--> 10
+    |
+    |-> ● if
+        |--> ● >
+        |    |
+        |    |--> ● x
+        |    |--> ● 5
+        |
+        |--> ● print
+        |    |--> ● "large"
+        |--> ● print
+             |--> ● "small"
+*/
+
+// Takes a string as input and returns and object containing the data structure for the
+// expression at the start of the string, along with the part of the string left after
+// parsing this expression.
+
+function parseExpression(program) {
+  program = skipSpace(program);
+  let match, expr;
+  if ((match = /^"([^"]*)"/.exec(program))) {
+    expr = { type: "value", value: match[1] };
+  } else if ((match = /^\d+\b/.exec(program))) {
+    expr = { type: "value", value: Number(match[0]) };
+  } else if ((match = /^[^\s(),#"]+/.exec(program))) {
+    expr = { type: "word", name: match[0] };
+  } else {
+    throw new SyntaxError("Unexpected syntax: " + program);
+  }
+  return parseApply(expr, program.slice(match[0].length));
+}
+
+function skipSpace(string) {
+    let first = string.search(/\S/);
+    if (first == -1) return ""
+    return string.slice(first);
+}
+
+function parseApply(expr, program) {
+  program = skipSpace(program);
+  if (program[0] != "(") {
+    return { expr: expr, rest: program };
+  }
+  program = skipSpace(program.slice(1));
+  expr = { type: "apply", operator: expr, args: [] };
+  while (program[0] != ")") {
+    let arg = parseExpression(program);
+    expr.args.push(arg.expr);
+    program = skipSpace(arg.rest);
+    if (program[0] == ",") {
+      program = skipSpace(program.slice(1));
+    } else if (program[0] != ")") {
+      throw new SyntaxError("Expected ',' or ')'");
+    }
+  }
+  return parseApply(expr, program.slice(1));
+}
+
+function parse(program) {
+  let { expr, rest } = parseExpression(program);
+  if (skipSpace(rest).length > 0) {
+    throw new SyntaxError("Unexpected text after program");
+  }
+  return expr;
+}
+
+console.log(parse("+(a, 10)"));
