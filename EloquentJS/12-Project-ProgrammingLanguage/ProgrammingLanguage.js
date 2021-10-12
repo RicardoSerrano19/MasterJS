f26fa4dc8181323a2681fe35b3f19484b895b1c7
Ricardo Serrano <ricardo.ss@live.com>
2021-10-12 15:13:35 -0500

    The evaluator

--- EloquentJS/12-Project-ProgrammingLanguage/ProgrammingLanguage.js
+++ EloquentJS/12-Project-ProgrammingLanguage/ProgrammingLanguage.js
@@ -113,4 +113,34 @@ function parse(program) {
   return expr;
 }
 
-console.log(parse("+(a, 10)"));
+console.log(parse(">(a, 10)"));
+
+/*
+    # 🔴 The evaluator
+*/
+
+
+const specialForms = Object.create(null);
+function evaluate(expr, scope) {
+  if (expr.type == "value") {
+    return expr.value;
+  } else if (expr.type == "word") {
+    if (expr.name in scope) {
+      return scope[expr.name];
+    } else {
+      throw new ReferenceError(`Undefined binding: ${expr.name}`);
+    }
+  } else if (expr.type == "apply") {
+    let { operator, args } = expr;
+    if (operator.type == "word" && operator.name in specialForms) {
+      return specialForms[operator.name](expr.args, scope);
+    } else {
+      let op = evaluate(operator, scope);
+      if (typeof op == "function") {
+        return op(...args.map(arg => evaluate(arg, scope)));
+      } else {
+        throw new TypeError("Applying a non-function.");
+      }
+    }
+  }
+}
