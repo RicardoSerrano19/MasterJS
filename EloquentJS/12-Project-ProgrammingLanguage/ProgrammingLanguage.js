abdc25e58df293e93823c55845c8dd24fd144b0e
Ricardo Serrano <ricardo.ss@live.com>
2021-10-18 14:53:20 -0500

    feat(chapter12): Special forms

--- EloquentJS/12-Project-ProgrammingLanguage/ProgrammingLanguage.js
+++ EloquentJS/12-Project-ProgrammingLanguage/ProgrammingLanguage.js
@@ -144,3 +144,48 @@ function evaluate(expr, scope) {
     }
   }
 }
+
+
+/*
+    # 🔴 Special forms
+*/
+
+
+specialForms.if = (args, scope) => {
+  if (args.length != 3) {
+    throw new SyntaxError("Wrong number of args to if");
+  } else if (evaluate(args[0], scope) !== false) {
+    return evaluate(args[1], scope);
+  } else {
+    return evaluate(args[2], scope);
+  }
+};
+
+specialForms.while = (args, scope) => {
+  if (args.length != 2) {
+    throw new SyntaxError("Wrong number of args to while");
+  }
+  while (evaluate(args[0], scope) !== false) {
+    evaluate(args[1], scope);
+  }
+  // Since undefined does not exist in Egg, we return false,
+  // for lack of a meaningful result.
+  return false;
+};
+
+specialForms.do = (args, scope) => {
+  let value = false;
+  for (let arg of args) {
+    value = evaluate(arg, scope);
+  }
+  return value;
+};
+
+specialForms.define = (args, scope) => {
+  if (args.length != 2 || args[0].type != "word") {
+    throw new SyntaxError("Incorrect use of define");
+  }
+  let value = evaluate(args[1], scope);
+  scope[args[0].name] = value;
+  return value;
+};
