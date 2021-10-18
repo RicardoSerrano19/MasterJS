c7bec2014d7b907fa37561727d447a485ce2822f
Ricardo Serrano <ricardo.ss@live.com>
2021-10-18 14:58:39 -0500

    feat(chapter12): The environment

--- EloquentJS/12-Project-ProgrammingLanguage/ProgrammingLanguage.js
+++ EloquentJS/12-Project-ProgrammingLanguage/ProgrammingLanguage.js
@@ -189,3 +189,37 @@ specialForms.define = (args, scope) => {
   scope[args[0].name] = value;
   return value;
 };
+
+
+/*
+    # 🔴 The environment
+*/
+
+const topScope = Object.create(null);
+topScope.true = true;
+topScope.false = false;
+
+let prog = parse(`if(true, false, true)`);
+console.log(evaluate(prog, topScope));
+
+for (let op of ["+", "-", "*", "/", "==", "<", ">"]) {
+  topScope[op] = Function("a, b", `return a ${op} b;`);
+}
+
+topScope.print = value => {
+  console.log(value);
+  return value;
+};
+
+function run(program) {
+  return evaluate(parse(program), Object.create(topScope));
+}
+
+run(`
+do(define(total, 0),
+define(count, 1),
+while(<(count, 11),
+do(define(total, +(total, count)),
+define(count, +(count, 1)))),
+print(total))
+`);
