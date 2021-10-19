1efbf41d600763d14850c4b37e3c7b4f7f728362
Ricardo Serrano <ricardo.ss@live.com>
2021-10-19 13:09:39 -0500

    feat(chapter13): The Web

--- EloquentJS/13-JavaScriptAndTheBrowser/JavaScriptAndTheBrowser.js
+++ EloquentJS/13-JavaScriptAndTheBrowser/JavaScriptAndTheBrowser.js
@@ -15,3 +15,20 @@
     Another computer can then establish a connection by connecting to the target machine using the correct port number. If the target machine can be reached and is listening on that port, the connection is successfully created. The listening computer is called the *server* and the connecting computer is called the *client*
     
 */
+
+
+/* 📃 The web */
+
+/*
+    The World Wide Web is a set of protocols and formats allow us to visit web page in a browser.
+    To become part of the Web, all you need to do is connect a machine to Internet and have it listen on port 80 with the HTTP protocol so that other computers can ask it for documents.
+
+    Each document on the Web is named by *Uniform Resource Locator* (URL), wich looks something like this:
+
+    -> https://www.google.com/13_browser.html
+    |protocol|    server    |     path     |
+
+    1.- Protocol: Which protocol is used.
+    2.- Server: Wich server is requesting the document from.
+    3.- Path: String that identifies the specific document(or resource) we are interested in.
+*/
